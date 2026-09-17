f65551781064068d823219b104d37303a07f86e6
Hannah Bejo <hannahbejo123@gmail.com>
2026-09-17 14:54:27 +0800

    Revised for localhost/memoriasys i guess

--- api/notallowed.php
+++ api/notallowed.php
@@ -2,6 +2,6 @@
 
 if (!defined('ITS_ME_JUSTTOVERIFY')) {
     http_response_code(404);
-    require $_SERVER['DOCUMENT_ROOT'] . '/404.html'; // Load a custom 404 page '/memoria/404.html If not in rootest root folder LOL'
+    require $_SERVER['DOCUMENT_ROOT'] . '/MemoriaSys/404.html'; //'/404.html'; // Load a custom 404 page '/memoria/404.html If not in rootest root folder LOL'
     exit();
 }
