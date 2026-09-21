f8440cb30968c89d9204aead46efa800435fd6b9
Fran <tabigsbolantoy@gmail.com>
2026-09-21 14:34:37 +0800

    not allowed phps for different URLS i guess hehe, just change the filename to notallowed.php on what hehe

---
+++ api/notallowed.laragon.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('ITS_ME_JUSTTOVERIFY')) {
+    http_response_code(404);
+    require $_SERVER['DOCUMENT_ROOT'] . '/404.html'; //'/MemoriaSys/404.html'; //'/404.html'; // Load a custom 404 page '/memoria/404.html If not in rootest root folder LOL'
+    exit();
+}
---
+++ api/notallowed.xampp.php
@@ -0,0 +1,8 @@
+<?php
+// xampp your set-up i guess haha
+
+if (!defined('ITS_ME_JUSTTOVERIFY')) {
+    http_response_code(404);
+    require $_SERVER['DOCUMENT_ROOT'] . '/MemoriaSys/404.html'; //'/404.html'; // Load a custom 404 page '/memoria/404.html If not in rootest root folder LOL'
+    exit();
+}
